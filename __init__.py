252bbe5e09f43f534082f00a3e83ff846f3d0e75
camcamslt <116881944+camcamslt@users.noreply.github.com>
2025-12-03 09:52:20 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -15,7 +15,7 @@ def hello_world():
 def MaPremiereAPI():
     return "<h2>Ma page de contact</h2>"
   
-  @app.route('/tawarano/')
+@app.route('/tawarano/')
 def meteo():
     response = urlopen('https://samples.openweathermap.org/data/2.5/forecast?lat=0&lon=0&appid=xxx')
     raw_content = response.read()
