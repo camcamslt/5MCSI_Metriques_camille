3fa783479b6a694cd1068bffca7432f911ca723b
camcamslt <116881944+camcamslt@users.noreply.github.com>
2025-12-03 10:47:08 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -11,9 +11,9 @@ app = Flask(__name__)
 def hello_world():
     return render_template('hello.html')
 
-@app.route("/contact/")
-def MaPremiereAPI():
-    return "<h2>Ma page de contact</h2>"
+@app.route('/contact/')
+def contact():
+    return render_template("contact.html")
   
 @app.route('/tawarano/')
 def meteo():
